43dbd84dbce8069763eb05551c9cce75b8ca3d58
Alphatule <66392548+alphatule@users.noreply.github.com>
2024-04-16 17:42:24 +0200

    Creado menu con una unica salida de escape

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int selection = 0;
+            System.out.println("Welcome to Three In a row\n" +
+                    "1. New Game\n" +
+                    "2. Load Game\n" +
+                    "3. Configuration\n" +
+                    "4. Exit\n\n" +
+                    "Select a option:");
+            try {
+                selection = Integer.valueOf(sc.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid integer input");
+            }
+            switch (selection) {
+                case 1:
+                    // New Game
+                    break;
+                case 2:
+                    // Load Game
+                    break;
+                case 3:
+                    // Configuration
+                    break;
+                case 4:
+                    // Exit
+                    break;
+                default:
+                    // Cualquier otro numero
+            }
+        }
     }
 }
