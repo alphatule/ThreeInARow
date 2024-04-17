8d48e3ba6cd8c50f7726eeb8ecdfa45f05b1a404
Alphatule <66392548+alphatule@users.noreply.github.com>
2024-04-17 19:58:09 +0200

    Creamos metodos para la nueva clase e integramos el menu

---
+++ src/TUI.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class TUI {
+    private void newGame() {
+        // Empezamos el juego
+        System.out.println("You chose: New Game");
+    }
+    private void loadGame() {
+        // Cargamos juego / partida
+        System.out.println("You chose: Load Game");
+    }
+    private void configuration() {
+        // Configuracion del juego
+        System.out.println("You chose: Configuration");
+    }
+    private void exit() {
+        // Salimos/acabamos la aplicacion
+        System.out.println("You chose: Exit");
+    }
+    public short menu(){
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int selection = 0;
+            System.out.println("Welcome to Three In a row\n" +
+                    "1. New Game\n" +
+                    "2. Load Game\n" +
+                    "3. Configuration\n" +
+                    "4. Exit\n\n" +
+                    "Select a option:");
+            try {
+                selection = Integer.valueOf(sc.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid integer input");
+            }
+            switch (selection) {
+                case 1:
+                    // New Game
+                    this.newGame();
+                    break;
+                case 2:
+                    // Load Game
+                    this.loadGame();
+                    break;
+                case 3:
+                    // Configuration
+                    this.configuration();
+                    break;
+                case 4:
+                    // Exit
+                    this.exit();
+                    break;
+                default:
+                    // Cualquier otro numero
+                    // seguira dando vueltas en el bucle
+            }
+        }
+    }
+}
