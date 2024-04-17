655ac94ddd661903fc4766f1f1313e0851259e79
Alphatule <66392548+alphatule@users.noreply.github.com>
2024-04-17 19:57:33 +0200

    Retiramos cosas del Main

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            int selection = 0;
-            System.out.println("Welcome to Three In a row\n" +
-                    "1. New Game\n" +
-                    "2. Load Game\n" +
-                    "3. Configuration\n" +
-                    "4. Exit\n\n" +
-                    "Select a option:");
-            try {
-                selection = Integer.valueOf(sc.nextLine());
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid integer input");
-            }
-            switch (selection) {
-                case 1:
-                    // New Game
-                    System.out.println("You chose: New Game");
-                    break;
-                case 2:
-                    // Load Game
-                    System.out.println("You chose: Load Game");
-                    break;
-                case 3:
-                    // Configuration
-                    System.out.println("You chose: Configuration");
-                    break;
-                case 4:
-                    // Exit
-                    System.out.println("You chose: Exit");
-                    break;
-                default:
-                    // Cualquier otro numero
-            }
-        }
+        TUI tui = new TUI();
+
     }
 }
