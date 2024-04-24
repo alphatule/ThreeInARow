8ddf6f00db9634c07e2b1e8c976ae6bc3ee96c96
Alphatule <66392548+alphatule@users.noreply.github.com>
2024-04-24 19:25:32 +0200

    Reesctructuracion de clases y codigo (Main)

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,60 @@
+import com.sun.org.apache.bcel.internal.generic.FALOAD;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import java.util.Scanner;
 
 public class Main {
+    static boolean enMenu = true;
     public static void main(String[] args) {
         TUI tui = new TUI();
-        tui.menu();
+        Juego juego = new Juego();
+
+        while (enMenu)
+        {
+            try
+            {
+                switch (tui.mostrarMenu()) {
+                    case 1:
+                        // New Game
+                        nuevaPartida();
+                    case 2:
+                        // Load Game
+                        cargarPartida();
+                    case 3:
+                        // Configuration
+                        configuracion();
+                    case 4:
+                        // Exit
+                        salir();
+                        break;
+                    default:
+                        // Cualquier otro numero
+                        // seguira dando vueltas en el bucle
+                }
+            } catch (NumberFormatException e)
+            {
+                // Volvemos al menu de nuevo :)
+            }
+        }
+    }
+
+    private static void nuevaPartida()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void cargarPartida()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void configuracion()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void salir()
+    {
+        enMenu = false;
     }
 }
