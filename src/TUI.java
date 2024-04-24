af56e98293bacb9fe8ce1d5b3ee32118f18befc1
Alphatule <66392548+alphatule@users.noreply.github.com>
2024-04-24 19:41:46 +0200

    Funcion mostrarTablero ready

--- src/TUI.java
+++ src/TUI.java
@@ -18,9 +18,29 @@ public class TUI {
     }
 
     // Muestra el tablero al completo y indica el turno del jugador
-    public void mostrarTablero(char[][] tablero, short jugador)
+    public void mostrarTablero(short[][] tablero, short jugador)
     {
-        throw new NotImplementedException();
+        System.out.println("tablero.length: "+tablero.length);
+        // Primer bucle de fila
+        // Segundo bucle de columnas
+        for (int fila = 0; fila < tablero.length; fila++) {
+            for (int columna = 0; columna < tablero[fila].length; columna++) {
+                if (tablero[fila][columna] == 0)
+                {
+                    // Posicion vacia
+                    System.out.print(0);
+                } else if (tablero[fila][columna] == 1)
+                {
+                    // Posicion de jugador 1
+                    System.out.print("X");
+                } else if (tablero[fila][columna] == 2)
+                {
+                    // posicion de jugador 2
+                    System.out.print("B");
+                }
+            }
+            System.out.println();
+        }
     }
 
     // Devuelve la casilla fila y columna de donde se ha colocado la nueva jugada
