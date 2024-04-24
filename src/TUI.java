42f6e3d6035377d455f174e0ace20e583bc0f65d
Alphatule <66392548+alphatule@users.noreply.github.com>
2024-04-24 19:14:34 +0200

    Reesctructuracion de clases y codigo (TUI)

--- src/TUI.java
+++ src/TUI.java
@@ -1,59 +1,38 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import java.util.Scanner;
 
 public class TUI {
-    private void newGame() {
-        // Empezamos el juego
-        System.out.println("You chose: New Game");
-    }
-    private void loadGame() {
-        // Cargamos juego / partida
-        System.out.println("You chose: Load Game");
-    }
-    private void configuration() {
-        // Configuracion del juego
-        System.out.println("You chose: Configuration");
-    }
-    private void exit() {
-        // Salimos/acabamos la aplicacion
-        System.out.println("You chose: Exit");
-    }
-    public short menu(){
-        Scanner sc = new Scanner(System.in);
-        int selection = 0;
-        // Mostramos menu
+    // Creamos una instancia hacia el scanner
+    Scanner sc = new Scanner(System.in);
+
+    // Mostramos menu y devolvemos la opcion seleccionada
+    public short mostrarMenu()
+    {
         System.out.println("Welcome to Three In a row\n" +
                 "1. New Game\n" +
                 "2. Load Game\n" +
                 "3. Configuration\n" +
                 "4. Exit\n\n" +
                 "Select a option:");
-        try {
-            selection = Integer.parseInt(sc.nextLine());
-            switch (selection) {
-                case 1:
-                    // New Game
-                    this.newGame();
-                    return 1;
-                case 2:
-                    // Load Game
-                    this.loadGame();
-                    return 2;
-                case 3:
-                    // Configuration
-                    this.configuration();
-                    return 3;
-                case 4:
-                    // Exit
-                    this.exit();
-                    return 4;
-                default:
-                    // Cualquier otro numero
-                    // seguira dando vueltas en el bucle
-                    return this.menu();
-            }
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid integer input");
-            return this.menu();
-        }
+        return (short) Integer.parseInt(sc.nextLine());
+    }
+
+    // Muestra el tablero al completo y indica el turno del jugador
+    public void mostrarTablero(char[][] tablero, short jugador)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Devuelve la casilla fila y columna de donde se ha colocado la nueva jugada
+    // aparte de pedirle al jugador la jugada actual.
+    public short[] recogerJugada()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Indica el ganador de la partida o empate
+    public char[][] finDePartida()
+    {
+        throw new NotImplementedException();
     }
 }
