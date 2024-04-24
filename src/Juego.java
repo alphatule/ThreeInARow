1bb4ba6903a003151d84e5ce34be8c51c42101bb
Alphatule <66392548+alphatule@users.noreply.github.com>
2024-04-24 19:14:56 +0200

    Reesctructuracion de clases y codigo (Juego)

---
+++ src/Juego.java
@@ -0,0 +1,30 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class Juego {
+    // Turno = False => Es turno de la maquina
+    // Turno = True => Es turno del jugador
+    boolean turno;
+    // El tablero deberia ser siempre un 3x3 pero podria variar para ampliar funcionalidad al juego.
+    char[][] tablero;
+
+    // Creamos un nuevo tablero en blanco
+    public void nuevaPartida()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Indicamos la fila y columna donde el jugador (que tiene el turno) quiere colocar la ficha
+    // por lo tanto deberiamos actualizar el tablero y el turno
+    public void jugar(short fila, short columna)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Recogemos una fila y columna comrpobamos si es una jugada ganadora;
+    // Devolvemos un booleano para saber si esta jugada es ganadora;
+    // Hay que tener en cuenta que el tablero y el turno no se alteral
+    public boolean jugadaGanadora(short fila, short columna)
+    {
+        throw new NotImplementedException();
+    }
+}
